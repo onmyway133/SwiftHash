367a0f64f2dae420c3a70b7c77489b36e9727202
Khoa Pham <onmyway133@gmail.com>
2016-07-04 20:21:47 +0200

    Use unwrap instead of nil coalescing unwrap optional

--- Sources/MD5.swift
+++ Sources/MD5.swift
@@ -72,7 +72,7 @@ func rstr2binl(input: [CUnsignedChar]) -> [Int32] {
   for i in 0.stride(to: input.count * 8, by: 8) {
     let value: Int32 = (Int32(input[i/8]) & 0xFF) << (Int32(i) % 32)
 
-    output[i >> 5] = (output[i >> 5] ?? 0) | value
+    output[i >> 5] = unwrap(output[i >> 5]) | value
   }
 
   return dictionary2array(output)
@@ -155,11 +155,11 @@ func binl_md5(input: [Int32], _ len: Int) -> [Int32] {
   }
 
   let value: Int32 = 0x80 << Int32((len) % 32)
-  x[len >> 5] = (x[len >> 5] ?? 0) | value
+  x[len >> 5] = unwrap(x[len >> 5]) | value
 
   // >>> 9
   let index = (((len + 64) >> 9) << 4) + 14
-  x[index] = (x[index] ?? 0) | Int32(len)
+  x[index] = unwrap(x[index]) | Int32(len)
 
   var a: Int32 =  1732584193
   var b: Int32 = -271733879
@@ -172,73 +172,73 @@ func binl_md5(input: [Int32], _ len: Int) -> [Int32] {
     let oldc: Int32 = c
     let oldd: Int32 = d
 
-    a = md5_ff(a, b, c, d, x[i + 0] ?? 0, 7 , -680876936)
-    d = md5_ff(d, a, b, c, x[i + 1] ?? 0, 12, -389564586)
-    c = md5_ff(c, d, a, b, x[i + 2] ?? 0, 17,  606105819)
-    b = md5_ff(b, c, d, a, x[i + 3] ?? 0, 22, -1044525330)
-    a = md5_ff(a, b, c, d, x[i + 4] ?? 0, 7 , -176418897)
-    d = md5_ff(d, a, b, c, x[i + 5] ?? 0, 12,  1200080426)
-    c = md5_ff(c, d, a, b, x[i + 6] ?? 0, 17, -1473231341)
-    b = md5_ff(b, c, d, a, x[i + 7] ?? 0, 22, -45705983)
-    a = md5_ff(a, b, c, d, x[i + 8] ?? 0, 7 ,  1770035416)
-    d = md5_ff(d, a, b, c, x[i + 9] ?? 0, 12, -1958414417)
-    c = md5_ff(c, d, a, b, x[i + 10] ?? 0, 17, -42063)
-    b = md5_ff(b, c, d, a, x[i + 11] ?? 0, 22, -1990404162)
-    a = md5_ff(a, b, c, d, x[i + 12] ?? 0, 7 ,  1804603682)
-    d = md5_ff(d, a, b, c, x[i + 13] ?? 0, 12, -40341101)
-    c = md5_ff(c, d, a, b, x[i + 14] ?? 0, 17, -1502002290)
-    b = md5_ff(b, c, d, a, x[i + 15] ?? 0, 22,  1236535329)
+    a = md5_ff(a, b, c, d, unwrap(x[i + 0]), 7 , -680876936)
+    d = md5_ff(d, a, b, c, unwrap(x[i + 1]), 12, -389564586)
+    c = md5_ff(c, d, a, b, unwrap(x[i + 2]), 17,  606105819)
+    b = md5_ff(b, c, d, a, unwrap(x[i + 3]), 22, -1044525330)
+    a = md5_ff(a, b, c, d, unwrap(x[i + 4]), 7 , -176418897)
+    d = md5_ff(d, a, b, c, unwrap(x[i + 5]), 12,  1200080426)
+    c = md5_ff(c, d, a, b, unwrap(x[i + 6]), 17, -1473231341)
+    b = md5_ff(b, c, d, a, unwrap(x[i + 7]), 22, -45705983)
+    a = md5_ff(a, b, c, d, unwrap(x[i + 8]), 7 ,  1770035416)
+    d = md5_ff(d, a, b, c, unwrap(x[i + 9]), 12, -1958414417)
+    c = md5_ff(c, d, a, b, unwrap(x[i + 10]), 17, -42063)
+    b = md5_ff(b, c, d, a, unwrap(x[i + 11]), 22, -1990404162)
+    a = md5_ff(a, b, c, d, unwrap(x[i + 12]), 7 ,  1804603682)
+    d = md5_ff(d, a, b, c, unwrap(x[i + 13]), 12, -40341101)
+    c = md5_ff(c, d, a, b, unwrap(x[i + 14]), 17, -1502002290)
+    b = md5_ff(b, c, d, a, unwrap(x[i + 15]), 22,  1236535329)
 
-    a = md5_gg(a, b, c, d, x[i + 1] ?? 0, 5 , -165796510)
-    d = md5_gg(d, a, b, c, x[i + 6] ?? 0, 9 , -1069501632)
-    c = md5_gg(c, d, a, b, x[i + 11] ?? 0, 14,  643717713)
-    b = md5_gg(b, c, d, a, x[i + 0] ?? 0, 20, -373897302)
-    a = md5_gg(a, b, c, d, x[i + 5] ?? 0, 5 , -701558691)
-    d = md5_gg(d, a, b, c, x[i + 10] ?? 0, 9 ,  38016083)
-    c = md5_gg(c, d, a, b, x[i + 15] ?? 0, 14, -660478335)
-    b = md5_gg(b, c, d, a, x[i + 4] ?? 0, 20, -405537848)
-    a = md5_gg(a, b, c, d, x[i + 9] ?? 0, 5 ,  568446438)
-    d = md5_gg(d, a, b, c, x[i + 14] ?? 0, 9 , -1019803690)
-    c = md5_gg(c, d, a, b, x[i + 3] ?? 0, 14, -187363961)
-    b = md5_gg(b, c, d, a, x[i + 8] ?? 0, 20,  1163531501)
-    a = md5_gg(a, b, c, d, x[i + 13] ?? 0, 5 , -1444681467)
-    d = md5_gg(d, a, b, c, x[i + 2] ?? 0, 9 , -51403784)
-    c = md5_gg(c, d, a, b, x[i + 7] ?? 0, 14,  1735328473)
-    b = md5_gg(b, c, d, a, x[i + 12] ?? 0, 20, -1926607734)
+    a = md5_gg(a, b, c, d, unwrap(x[i + 1]), 5 , -165796510)
+    d = md5_gg(d, a, b, c, unwrap(x[i + 6]), 9 , -1069501632)
+    c = md5_gg(c, d, a, b, unwrap(x[i + 11]), 14,  643717713)
+    b = md5_gg(b, c, d, a, unwrap(x[i + 0]), 20, -373897302)
+    a = md5_gg(a, b, c, d, unwrap(x[i + 5]), 5 , -701558691)
+    d = md5_gg(d, a, b, c, unwrap(x[i + 10]), 9 ,  38016083)
+    c = md5_gg(c, d, a, b, unwrap(x[i + 15]), 14, -660478335)
+    b = md5_gg(b, c, d, a, unwrap(x[i + 4]), 20, -405537848)
+    a = md5_gg(a, b, c, d, unwrap(x[i + 9]), 5 ,  568446438)
+    d = md5_gg(d, a, b, c, unwrap(x[i + 14]), 9 , -1019803690)
+    c = md5_gg(c, d, a, b, unwrap(x[i + 3]), 14, -187363961)
+    b = md5_gg(b, c, d, a, unwrap(x[i + 8]), 20,  1163531501)
+    a = md5_gg(a, b, c, d, unwrap(x[i + 13]), 5 , -1444681467)
+    d = md5_gg(d, a, b, c, unwrap(x[i + 2]), 9 , -51403784)
+    c = md5_gg(c, d, a, b, unwrap(x[i + 7]), 14,  1735328473)
+    b = md5_gg(b, c, d, a, unwrap(x[i + 12]), 20, -1926607734)
 
-    a = md5_hh(a, b, c, d, x[i + 5] ?? 0, 4 , -378558)
-    d = md5_hh(d, a, b, c, x[i + 8] ?? 0, 11, -2022574463)
-    c = md5_hh(c, d, a, b, x[i + 11] ?? 0, 16,  1839030562)
-    b = md5_hh(b, c, d, a, x[i + 14] ?? 0, 23, -35309556)
-    a = md5_hh(a, b, c, d, x[i + 1] ?? 0, 4 , -1530992060)
-    d = md5_hh(d, a, b, c, x[i + 4] ?? 0, 11,  1272893353)
-    c = md5_hh(c, d, a, b, x[i + 7] ?? 0, 16, -155497632)
-    b = md5_hh(b, c, d, a, x[i + 10] ?? 0, 23, -1094730640)
-    a = md5_hh(a, b, c, d, x[i + 13] ?? 0, 4 ,  681279174)
-    d = md5_hh(d, a, b, c, x[i + 0] ?? 0, 11, -358537222)
-    c = md5_hh(c, d, a, b, x[i + 3] ?? 0, 16, -722521979)
-    b = md5_hh(b, c, d, a, x[i + 6] ?? 0, 23,  76029189)
-    a = md5_hh(a, b, c, d, x[i + 9] ?? 0, 4 , -640364487)
-    d = md5_hh(d, a, b, c, x[i + 12] ?? 0, 11, -421815835)
-    c = md5_hh(c, d, a, b, x[i + 15] ?? 0, 16,  530742520)
-    b = md5_hh(b, c, d, a, x[i + 2] ?? 0, 23, -995338651)
+    a = md5_hh(a, b, c, d, unwrap(x[i + 5]), 4 , -378558)
+    d = md5_hh(d, a, b, c, unwrap(x[i + 8]), 11, -2022574463)
+    c = md5_hh(c, d, a, b, unwrap(x[i + 11]), 16,  1839030562)
+    b = md5_hh(b, c, d, a, unwrap(x[i + 14]), 23, -35309556)
+    a = md5_hh(a, b, c, d, unwrap(x[i + 1]), 4 , -1530992060)
+    d = md5_hh(d, a, b, c, unwrap(x[i + 4]), 11,  1272893353)
+    c = md5_hh(c, d, a, b, unwrap(x[i + 7]), 16, -155497632)
+    b = md5_hh(b, c, d, a, unwrap(x[i + 10]), 23, -1094730640)
+    a = md5_hh(a, b, c, d, unwrap(x[i + 13]), 4 ,  681279174)
+    d = md5_hh(d, a, b, c, unwrap(x[i + 0]), 11, -358537222)
+    c = md5_hh(c, d, a, b, unwrap(x[i + 3]), 16, -722521979)
+    b = md5_hh(b, c, d, a, unwrap(x[i + 6]), 23,  76029189)
+    a = md5_hh(a, b, c, d, unwrap(x[i + 9]), 4 , -640364487)
+    d = md5_hh(d, a, b, c, unwrap(x[i + 12]), 11, -421815835)
+    c = md5_hh(c, d, a, b, unwrap(x[i + 15]), 16,  530742520)
+    b = md5_hh(b, c, d, a, unwrap(x[i + 2]), 23, -995338651)
 
-    a = md5_ii(a, b, c, d, x[i + 0] ?? 0, 6 , -198630844)
-    d = md5_ii(d, a, b, c, x[i + 7] ?? 0, 10,  1126891415)
-    c = md5_ii(c, d, a, b, x[i + 14] ?? 0, 15, -1416354905)
-    b = md5_ii(b, c, d, a, x[i + 5] ?? 0, 21, -57434055)
-    a = md5_ii(a, b, c, d, x[i + 12] ?? 0, 6 ,  1700485571)
-    d = md5_ii(d, a, b, c, x[i + 3] ?? 0, 10, -1894986606)
-    c = md5_ii(c, d, a, b, x[i + 10] ?? 0, 15, -1051523)
-    b = md5_ii(b, c, d, a, x[i + 1] ?? 0, 21, -2054922799)
-    a = md5_ii(a, b, c, d, x[i + 8] ?? 0, 6 ,  1873313359)
-    d = md5_ii(d, a, b, c, x[i + 15] ?? 0, 10, -30611744)
-    c = md5_ii(c, d, a, b, x[i + 6] ?? 0, 15, -1560198380)
-    b = md5_ii(b, c, d, a, x[i + 13] ?? 0, 21,  1309151649)
-    a = md5_ii(a, b, c, d, x[i + 4] ?? 0, 6 , -145523070)
-    d = md5_ii(d, a, b, c, x[i + 11] ?? 0, 10, -1120210379)
-    c = md5_ii(c, d, a, b, x[i + 2] ?? 0, 15,  718787259)
-    b = md5_ii(b, c, d, a, x[i + 9] ?? 0, 21, -343485551)
+    a = md5_ii(a, b, c, d, unwrap(x[i + 0]), 6 , -198630844)
+    d = md5_ii(d, a, b, c, unwrap(x[i + 7]), 10,  1126891415)
+    c = md5_ii(c, d, a, b, unwrap(x[i + 14]), 15, -1416354905)
+    b = md5_ii(b, c, d, a, unwrap(x[i + 5]), 21, -57434055)
+    a = md5_ii(a, b, c, d, unwrap(x[i + 12]), 6 ,  1700485571)
+    d = md5_ii(d, a, b, c, unwrap(x[i + 3]), 10, -1894986606)
+    c = md5_ii(c, d, a, b, unwrap(x[i + 10]), 15, -1051523)
+    b = md5_ii(b, c, d, a, unwrap(x[i + 1]), 21, -2054922799)
+    a = md5_ii(a, b, c, d, unwrap(x[i + 8]), 6 ,  1873313359)
+    d = md5_ii(d, a, b, c, unwrap(x[i + 15]), 10, -30611744)
+    c = md5_ii(c, d, a, b, unwrap(x[i + 6]), 15, -1560198380)
+    b = md5_ii(b, c, d, a, unwrap(x[i + 13]), 21,  1309151649)
+    a = md5_ii(a, b, c, d, unwrap(x[i + 4]), 6 , -145523070)
+    d = md5_ii(d, a, b, c, unwrap(x[i + 11]), 10, -1120210379)
+    c = md5_ii(c, d, a, b, unwrap(x[i + 2]), 15,  718787259)
+    b = md5_ii(b, c, d, a, unwrap(x[i + 9]), 21, -343485551)
 
     a = safe_add(a, olda)
     b = safe_add(b, oldb)
@@ -259,10 +259,18 @@ func dictionary2array(dictionary: [Int: Int32]) -> [Int32] {
   var array = Array<Int32>(count: dictionary.keys.count, repeatedValue: 0)
 
   for i in Array(dictionary.keys).sort() {
-    array[i] = dictionary[i] ?? 0
+    array[i] = unwrap(dictionary[i])
   }
 
   return array
+}
+
+func unwrap(value: Int32?, _ fallback: Int32 = 0) -> Int32 {
+  if let value = value {
+    return value
+  }
+
+  return fallback
 }
 
 func zeroFillRightShift(num: Int32, _ count: Int32) -> Int32 {
